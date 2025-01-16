b941f55820eec5c53ac465350b0c01490eb0a8d1
Itsuki54 <5shi.icyu@gmail.com>
2025-01-16 20:39:36 +0900

    add:hono

--- apps/api/src/lib/hono.ts
+++ apps/api/src/lib/hono.ts
@@ -2,4 +2,4 @@ import { hc } from 'hono/client';
 
 import type { AppType } from '../index.ts';
 
-export const client = hc<AppType>(process.env.API_URL!);
+export const client = hc<AppType>(process.env['API_URL']!);
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,29 +1,5 @@
 import { client } from '@repo/api/src/lib/hono';
-import { Card } from '@repo/ui/card';
-
-const LINKS = [
-  {
-    title: 'Docs',
-    href: 'https://turbo.build/repo/docs',
-    description: 'Find in-depth information about Turborepo features and API.',
-  },
-  {
-    title: 'Learn',
-    href: 'https://turbo.build/repo/docs/handbook',
-    description: 'Learn more about monorepos with our handbook.',
-  },
-  {
-    title: 'Templates',
-    href: 'https://turbo.build/repo/docs/getting-started/from-example',
-    description: 'Choose from over 15 examples and deploy with a single click.',
-  },
-  {
-    title: 'Deploy',
-    href: 'https://vercel.com/new',
-    description:
-      'Instantly deploy your Turborepo to a shareable URL with Vercel.',
-  },
-];
+import { Button } from '@repo/ui/button';
 
 export default function Page({
   text,
@@ -34,11 +10,7 @@ export default function Page({
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="font-bold text-6xl">{text}</h1>
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        {LINKS.map(({ title, href, description }) => (
-          <Card ref={href} key={title} title={title}>
-            {description}
-          </Card>
-        ))}
+        <Button className="lg:col-span-2">Button</Button>
       </div>
     </main>
   );
