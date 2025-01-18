8d69e7ed5253437219bc6ff0187180d85551a21e
Itsuki54 <5shi.icyu@gmail.com>
2025-01-18 12:03:01 +0900

    fix

--- packages/database/prisma/zod-prisma/user.ts
+++
@@ -1,9 +0,0 @@
-import * as z from 'zod';
-
-export const UserModel = z.object({
-  id: z.string(),
-  email: z.string(),
-  name: z.string().nullish(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
-});
