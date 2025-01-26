5ceb8008054d3e9a93e0cde977582b66a3ccebfa
Itsuki54 <5shi.icyu@gmail.com>
2025-01-26 14:31:07 +0900

    add:tsdoc

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,6 +2,9 @@ import '@/styles/globals.css';
 import '@repo/ui/styles.css';
 import type { AppProps } from 'next/app';
 
+/**
+ * Main application component
+ */
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
 };
--- packages/config-eslint/.eslintrc.js
+++ packages/config-eslint/.eslintrc.js
@@ -16,11 +16,13 @@ module.exports = {
     'prefer-arrow-functions',
     'import',
     'jsx-a11y',
+    'jsdoc',
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@next/next/recommended',
+    'plugin:jsdoc/recommended-typescript-error',
   ],
   rules: {
     'react/jsx-sort-props': 'error',
@@ -49,8 +51,88 @@ module.exports = {
       2,
       { namedComponents: 'arrow-function' },
     ],
+    'jsdoc/check-param-names': [
+      'error',
+      {
+        checkDestructured: false,
+      },
+    ],
+    'jsdoc/check-tag-names': [
+      'error',
+      {
+        definedTags: ['remarks', 'typeParam'],
+      },
+    ],
+    'jsdoc/require-description': [
+      'error',
+      {
+        contexts: [
+          'ArrowFunctionExpression',
+          'ClassDeclaration',
+          'ClassExpression',
+          'FunctionDeclaration',
+          'FunctionExpression',
+          'MethodDefinition',
+          'PropertyDefinition',
+          'VariableDeclaration',
+          'TSInterfaceDeclaration',
+          'TSTypeAliasDeclaration',
+          'TSPropertySignature',
+          'TSMethodSignature',
+        ],
+      },
+    ],
+    'jsdoc/require-hyphen-before-param-description': ['error', 'always'],
+    'jsdoc/require-jsdoc': [
+      'error',
+      {
+        publicOnly: true,
+        require: {
+          ArrowFunctionExpression: true,
+          ClassDeclaration: true,
+          ClassExpression: true,
+          FunctionDeclaration: true,
+          FunctionExpression: true,
+          MethodDefinition: true,
+        },
+        contexts: [
+          'PropertyDefinition',
+          'VariableDeclaration',
+          'TSInterfaceDeclaration',
+          'TSTypeAliasDeclaration',
+          'TSPropertySignature',
+          'TSMethodSignature',
+        ],
+        checkConstructors: false,
+      },
+    ],
+    'jsdoc/require-param': [
+      'error',
+      {
+        checkDestructuredRoots: false,
+      },
+    ],
+    'jsdoc/tag-lines': [
+      'error',
+      'always',
+      {
+        startLines: 1,
+        applyToEndTag: false,
+      },
+    ],
+    'jsdoc/sort-tags': [
+      'error',
+      {
+        reportIntraTagGroupSpacing: false,
+      },
+    ],
+    'jsdoc/require-returns': ['off'],
   },
-
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   overrides: [
     {
       files: ['./**'],
@@ -59,6 +141,15 @@ module.exports = {
         'no-undef': 'off',
         '@typescript-eslint/no-unused-vars': ['off'],
         '@typescript-eslint/no-explicit-any': ['off'],
+      },
+    },
+    {
+      files: ['*.js', '*.mjs', '*.cjs'],
+      rules: {
+        'jsdoc/require-param-type': ['error'],
+        'jsdoc/require-returns': ['error'],
+        'jsdoc/require-returns-type': ['error'],
+        'jsdoc/no-types': ['off'],
       },
     },
   ],
